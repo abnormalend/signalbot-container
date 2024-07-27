35db42200d6b35dd48df60c7731cd8534e4afae9
Brent Rogers <brentarogers@gmail.com>
2024-07-27 11:24:23 -0400

    adding to python file

--- signalbot.py
+++ signalbot.py
@@ -1,5 +1,11 @@
 from time import sleep
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+logger.info("starting python signal connector")
 
 while True:
     sleep(10)
-    print("python process")
+    logger.info("python process checkin")
