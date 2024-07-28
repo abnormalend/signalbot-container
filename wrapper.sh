305e6aeee1f3c08740f22e597fe7da24d1e372a2
Brent Rogers <brentarogers@gmail.com>
2024-07-28 11:19:09 -0400

    manual receive

--- wrapper.sh
+++ wrapper.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # Start signal daemon
-/usr/bin/signal-cli --config /config -a $ACCOUNT daemon --http 0.0.0.0:8551 &
+/usr/bin/signal-cli \
+            --config /config \
+            -a $ACCOUNT \
+            daemon \
+            --http 0.0.0.0:8551 \
+            --receive-mode=manual &
 
 # Start python handler
 # python3 /app/signalbot.py &
